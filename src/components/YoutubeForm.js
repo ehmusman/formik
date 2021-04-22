4a940413077420532d59b8d67e9efda432c7b693
ehmusman <ehmusman@gmail.com>
2021-04-23 02:40:30 +0500

    formik is installed and implemented to hold and change the form state

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,19 +1,45 @@
 import React from 'react'
-
+import { useFormik } from "formik"
 const YoutubeForm = () => {
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            email: "",
+            channel: ""
+        }
+    })
+    console.log(formik)
     return (
         <form className="container">
             <div className="form-group">
-                <lable htmlFor="name">Name</lable>
-                <input type="text" name="name" id="name" className="form-control" />
+                <label htmlFor="name">Name</label>
+                <input
+                    onChange={formik.handleChange}
+                    value={formik.values.name}
+                    type="text"
+                    name="name"
+                    id="name"
+                    className="form-control" />
             </div>
             <div className="form-group">
-                <lable htmlFor="email">Email</lable>
-                <input type="email" name="email" id="email" className="form-control" />
+                <label htmlFor="email">Email</label>
+                <input
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="form-control" />
             </div>
             <div className="form-group">
-                <lable htmlFor="channem">Channel</lable>
-                <input type="text" name="channem" id="channem" className="form-control" />
+                <label htmlFor="channel">Channel</label>
+                <input
+                    onChange={formik.handleChange}
+                    value={formik.values.channel}
+                    type="text"
+                    name="channel"
+                    id="channel"
+                    className="form-control" />
             </div>
             <button className="btn btn-primary btn-block">Submit</button>
         </form>
