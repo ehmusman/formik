94ccc927ebad19dd02fee6600196377048ea43a5
ehmusman <ehmusman@gmail.com>
2021-04-23 09:22:35 +0500

    Yup is installed its just like joi. errors are displayed successfully

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,16 +1,35 @@
 import React from 'react'
 import { useFormik } from "formik"
+import * as Yup from "yup"
+
+const initialValues = {
+    name: "",
+    email: "",
+    channel: ""
+}
+const onSubmit = values => {
+    console.log("form data", values)
+}
+
+const validationSchema = Yup.object().shape({
+    name: Yup.string().required("Name is Required"),
+    email: Yup.string()
+        .email("Invalid emaill formate")
+        .required("Email is Required"),
+    channel: Yup.string().required("Channel is Required")
+})
 const YoutubeForm = () => {
+
     const formik = useFormik({
-        initialValues: {
-            name: "",
-            email: "",
-            channel: ""
-        }
+        initialValues,
+        onSubmit,
+        validationSchema
+        // validate
     })
-    console.log(formik)
+    console.log("formik", formik)
+    console.log("visited", formik.touched)
     return (
-        <form className="container">
+        <form className="container" onSubmit={formik.handleSubmit}>
             <div className="form-group">
                 <label htmlFor="name">Name</label>
                 <input
@@ -19,7 +38,11 @@ const YoutubeForm = () => {
                     type="text"
                     name="name"
                     id="name"
-                    className="form-control" />
+                    onBlur={formik.handleBlur}
+                    className={`form-control ${formik.touched.name && formik.errors.name ? "is-invalid" : ""}`} />
+                <div className="invalid-feedback">
+                    {formik.errors.name}
+                </div>
             </div>
             <div className="form-group">
                 <label htmlFor="email">Email</label>
@@ -29,7 +52,11 @@ const YoutubeForm = () => {
                     type="email"
                     name="email"
                     id="email"
-                    className="form-control" />
+                    onBlur={formik.handleBlur}
+                    className={`form-control ${formik.touched.email && formik.errors.email ? "is-invalid" : ""}`} />
+                <div className="invalid-feedback">
+                    {formik.errors.email}
+                </div>
             </div>
             <div className="form-group">
                 <label htmlFor="channel">Channel</label>
@@ -39,9 +66,13 @@ const YoutubeForm = () => {
                     type="text"
                     name="channel"
                     id="channel"
-                    className="form-control" />
+                    onBlur={formik.handleBlur}
+                    className={`form-control ${formik.touched.channel && formik.errors.channel ? "is-invalid" : ""}`} />
+                <div className="invalid-feedback">
+                    {formik.errors.channel}
+                </div>
             </div>
-            <button className="btn btn-primary btn-block">Submit</button>
+            <button type="submit" className="btn btn-primary btn-block">Submit</button>
         </form>
     )
 }
